1281fd8c9dca6be23fdaa6b9390ec5f9a6d9bdee
Giulio Martena <giuliomariomartena@gmail.com>
2022-08-09 00:36:56 +0200

    Added data loading and parsing

---
+++ R/01_dataLoading.R
@@ -0,0 +1,10 @@
+vgames <- read_csv('data/vgsales.csv', show_col_types = FALSE, col_types = list(
+    Year = col_date("%Y")
+))
+
+vgames <- vgames %>%
+    mutate(
+        Platform = factor(Platform),
+        Genre = factor(Genre),
+        Publisher = factor(Publisher)
+    )
