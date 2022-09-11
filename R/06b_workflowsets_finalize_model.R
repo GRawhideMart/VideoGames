2d83463e3cb00fd83e11754df9692fb2f3842438
Giulio Mario Martena <giuliomariomartena@gmail.com>
2022-09-11 19:01:38 +0200

    Automatized best model selection

--- R/06b_workflowsets_finalize_model.R
+++ R/06b_workflowsets_finalize_model.R
@@ -3,12 +3,19 @@ vgames_models %>%
     autoplot(metric = "mae") %>%
     ggsave(filename = "graphs/03_VariousModelsComparison.png", device = "png")
 
+# Select best model ID
+vgames_best_model_id <- vgames_models %>%
+    rank_results(rank_metric = c("mae"), select_best = TRUE) %>%
+    filter(.metric == "mae") %>%
+    head(1) %>%
+    pull(wflow_id)
+
 # Select best model according to MAE
 best_model <- vgames_models %>%
-    extract_workflow_set_result("simple_nn") %>%
+    extract_workflow_set_result(vgames_best_model_id) %>%
     select_best(metric = "mae")
 
 # Finalize the model
 vgames_finalized_model <- vgames_models %>%
-    extract_workflow('simple_nn') %>%
+    extract_workflow(vgames_best_model_id) %>%
     finalize_workflow(best_model %>% select(-.config))
